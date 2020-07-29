5398d9259e5abf2b21f8145b3e44f9a81d3f763f
Rajat Ghoshal <rghoshal8989@gmail.com>
2020-07-29 12:46:04 +0530

    Added the notebook files

--- file.py
+++ file.py
@@ -1,2 +1,5 @@
 import os
-os.system('pwd')
+str2 = "19921002"
+str3 = "12.nc"
+str4 = "rsync -avzh /mnt/podaac_drive/allData/merged_alt/L4/cdr_grid/ssh_grids_v1812_"+str2+str3 +" /home/rghoshal/Desktop/ugpcode/"
+os.system(str4)
