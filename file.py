d15c200f13fd060edda587c2a13729b6bdca47af
Rajat Ghoshal <rghoshal8989@gmail.com>
2020-07-30 08:37:01 +0530

    Removed binary data and lab part

--- file.py
+++ file.py
@@ -1,5 +1,6 @@
 import os
-str2 = "19921002"
+str2 = "19921003"
 str3 = "12.nc"
-str4 = "rsync -avzh /mnt/podaac_drive/allData/merged_alt/L4/cdr_grid/ssh_grids_v1812_"+str2+str3 +" /home/rghoshal/Desktop/ugpcode/"
-os.system(str4)
+try:
+    str4 = "rsync -avzh /mnt/podaac_drive/allData/merged_alt/L4/cdr_grid/ssh_grids_v1812_"+str2+str3 +" /home/rghoshal/Desktop/ugpcode/"
+    os.system(str4)    
