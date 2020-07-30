80438ff1370b40fde26c181bd968cd30153ea376
Rajat Ghoshal <rghoshal8989@gmail.com>
2020-07-30 08:38:19 +0530

    Changed file.py

--- file.py
+++ file.py
@@ -1,6 +1,6 @@
+# Script for downloading ssh_grid data
 import os
 str2 = "19921003"
 str3 = "12.nc"
-try:
-    str4 = "rsync -avzh /mnt/podaac_drive/allData/merged_alt/L4/cdr_grid/ssh_grids_v1812_"+str2+str3 +" /home/rghoshal/Desktop/ugpcode/"
-    os.system(str4)    
+str4 = "rsync -avzh /mnt/podaac_drive/allData/merged_alt/L4/cdr_grid/ssh_grids_v1812_"+str2+str3 +" /home/rghoshal/Desktop/ugpcode/"
+os.system(str4) 
